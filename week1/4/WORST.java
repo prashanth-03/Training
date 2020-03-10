2b2187714d07bef34debef5a6521b08dfffbc9c2
prashanth-03 <55593652+prashanth-03@users.noreply.github.com>
2020-03-10 19:01:43 +0530

    Update WORST.java

--- week1/4/WORST.java
+++ week1/4/WORST.java
@@ -1,5 +1,5 @@
 public class WORST {            // will give wrong output
-    static int  min_replacements(int n)
+    static int  replacement1(int n,int a)
     {
             int c = 0;
             while (n != 1)
@@ -11,12 +11,16 @@ public class WORST {            // will give wrong output
                 }
                 else
                 {
-                    n -= 1;         // when will u do n+1 
+                    if(a==1)        // when will u do n+1 
+                    n -= 1; 
+                    else
+                      n+=1;  
                     c++;
                 }
             }
             return c;
     }
+      
     public static void main(String args[])
     {
         int []input={8,21,100,63,1,123,99,201};//INPUT DATA
@@ -24,11 +28,12 @@ public class WORST {            // will give wrong output
         //SOLUTION
         for(int i:input)
         {
-                int ans=min_replacements(i);
-                System.out.print(ans+" ");
+                int ans=min_replacements(i,0);
+                      int ans1=min_replacements(i,1);
+                System.out.print(Math.min(ans,ans1)+" ");
             }
         //EXPECTED OUTPUT
-        int []expected_out={3,6,8,10,0,11,9,10};
+        int []expected_out={3,6,8,7,0,9,9,10};
         System.out.println("\nExpected output:");
         for(int i:expected_out)
             System.out.print(i+" ");
