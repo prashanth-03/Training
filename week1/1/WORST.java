f4a2ffd3594abe76b376831d108c88403e7a0c44
mentor2.suntek <mentor2.suntek@gmail.com>
2020-03-11 20:59:13 +0530

    added review comments

--- week1/1/WORST.java
+++ week1/1/WORST.java
@@ -3,16 +3,16 @@ public class WORST {
         {
             //INPUT DATA
             int [] input={1,3,4,8};
-            int [][]query={{0,1},{1,2},{0,3},{3,3}};
+            int [][] query={{0,1},{1,2},{0,3},{3,3}};
             System.out.println("Actual output:");
             //SOLUTION
             for(int i=0;i<query.length;i++)
             {
-                int k=0;
-                for(int j= query[i][0];j<= query[i][1];j++)
+                int k=0;						// variable name inappropriate
+                for(int j= query[i][0];j<= query[i][1];j++)		// use curly braces
                     k^=input[j];
 		k=k^0;   
-             System.out.print(k+" ");
+             	System.out.print(k+" ");
             }
             //EXPECTED OUTPUT
             int []expected={2,7,14,8};
